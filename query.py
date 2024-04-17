bfcdb55fae802ae2d61d090201cedaeb3db1b170
Jieun Jeong <jieun.jeong09@gmail.com>
2024-04-16 22:20:48 -0400

    Add files via upload

---
+++ query.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import re
+#create DataFrame from resources file
+ClinVar = pd.read_csv('ClinInfo.txt.gz',delimiter='\t',compression='gzip')
+# chr	position	ref	allele	gene	sig	consequence
+# chr1	17018917	CA	C	SDHB	L__	frameshift_variant
+# ...
+# chr1	17018939	GC	G	SDHB	P__	frameshift_variant
+# ...
+# chr1	17018955	G	C	SDHB	_P_	missense_variant
+GnomAD = pd.read_csv('GnomAD_patho.txt.gz',delimiter='\t',compression='gzip')
+# chr	position	ref	allele	gene	VEP Annotation	ClinVarSig	AF_log10
+# chr1	17018815	C	T	SDHB	3_prime_UTR_variant		-6.0105
+# chr1	17018817	C	A	SDHB	3_prime_UTR_variant		-5.7187
+
+# merge resources
+ClinInfo = pd.merge(ClinVar,GnomAD,on=['chr','position','ref','allele'],how='left')
+ClinInfo.to_csv('ClinInfo.txt',sep='\t',index=False)
+
+import os
+os.system('gzip ClinInfo.txt')
+
+
+
